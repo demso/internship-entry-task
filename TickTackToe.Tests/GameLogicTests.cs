c1935a6eb350954e5d21091ed234c7db280270d7
dem <dmitriy313.k@gmail.com>
2025-07-27 12:30:09 +0300

    Add new test

--- TickTackToe.Tests/GameLogicTests.cs
+++ TickTackToe.Tests/GameLogicTests.cs
@@ -1,4 +1,6 @@
-﻿using TickTackToe.Api.Services;
+﻿using TickTackToe.Api.Entities.Game;
+using TickTackToe.Api.Enums;
+using TickTackToe.Api.Services;
 
 namespace TickTackToe.Tests;
 
@@ -12,49 +14,81 @@ namespace TickTackToe.Tests;
 // [Fact(Skip = "Reason")] // Пропуск теста
 
 public class GameLogicTests {
-    // private readonly GameService _gameService;
-    //
-    // public GameLogicTests()
-    // {
-    //     _gameService = new GameService();
-    // }
-    //
+    private readonly GameService _gameService;
+    
+    public GameLogicTests()
+    {
+        _gameService = new GameService();
+    }
+    
+    //проверить есть ли выигрышная комбинация в какой-либо клетке игры
+    bool CheckAllWinCondition(Game game) {
+        for (var row = 0; row < game.BoardSize; row++) {
+            for (var column = 0; column < game.BoardSize; column++) {
+                if (_gameService.CheckWinCondition(row, column, game.WhoseTurn.ToString(), game.WinCondition, game.BoardSize, game.Board))
+                    return true;
+            }
+        }
+        return false;
+    }
+    
+    [Fact]
+    public void CreateGame_ReturnsValidGame()
+    {
+        // Arrange
+        var game = _gameService.CreateGame(3,3);
+    
+        // Assert
+        Assert.NotNull(game);
+        Assert.Equal(Player.X, game.WhoseTurn);
+        Assert.Equal(0, game.TurnNumber);
+        Assert.Equal(GameResult.None, game.GameResult);
+        Assert.Equal(GameState.InProgress, game.GameState);
+        Assert.Equal(3, game.WinCondition);
+        Assert.Equal(3, game.BoardSize);
+        Assert.Equal(3, game.Board.Length);
+        Assert.Equal(3, game.Board[0].Length);
+        Assert.False(CheckAllWinCondition(game));
+        Assert.False(_gameService.CheckDraw(game.Board));
+        Assert.All(game.Board, row => Assert.All(row, cell => Assert.Equal("", cell))); 
+    }
     // [Fact]
-    // public void CreateNewGame_ReturnsValidGame()
-    // {
-    //     // Act
-    //     var game = _gameService.CreateNewGame();
-    //
-    //     // Assert
-    //     Assert.NotEqual(Guid.Empty, game.Id);
-    //     Assert.Equal("X", game.CurrentPlayer);
-    //     Assert.Equal(0, game.MoveCount);
-    //     Assert.Null(game.Winner);
-    //     Assert.False(game.IsDraw);
-    //     Assert.Equal(3, game.Board.Length);
-    //     Assert.All(game.Board, row => Assert.All(row, cell => Assert.Null(cell))); 
-    // }
-    // [Fact]
-    // public void MakeMove_ValidMove_UpdatesGameState()
+    // public void CheckWinCondition_ValidTurn_NoWin()
     // {
     //     // Arrange
-    //     var game = _gameService.CreateNewGame();
-    //     var position = new Position
-    //     {
-    //         Column = 0,
-    //         Row = 0
+    //     var board1 = new string[][] {
+    //         ["", "X", ""],
+    //         ["O", "X", ""],
+    //         ["", "X", ""]
+    //     };
+    //
+    //     var board2 = new string[][] {
+    //         ["", "", "X", "", "X"],
+    //         ["O", "", "", "", ""],
+    //         ["", "", "X", "", ""],
+    //         ["", "", "X", "", "O"],
+    //         ["X", "", "X", "", ""]
     //     };
     //     
+    //     var board3 = 
+    //     
+    //     var game = _gameService.CreateGame(3, 3);
+    //     var row = 1;
+    //     var column = 1;
     //
     //     // Act
-    //     _gameService.MakeMove(game, "X", position);
+    //     game.Board[row][column] = Player.X.ToString();
+    //     game.WhoseTurn = Player.O;
+    //     game.TurnNumber += 1;
     //
     //     // Assert
-    //     Assert.Equal("X", game.Board[0][0]);
-    //     Assert.Equal("O", game.CurrentPlayer);
-    //     Assert.Equal(1, game.MoveCount);
-    //     Assert.Null(game.Winner);
-    //     Assert.False(game.IsDraw);
+    //     Assert.Equal(Player.X.ToString(), game.Board[row][column]);
+    //     Assert.Equal(Player.O, game.WhoseTurn);
+    //     Assert.Equal(1, game.TurnNumber);
+    //     Assert.Equal(GameResult.None, game.GameResult);
+    //     Assert.Equal(GameState.InProgress, game.GameState);
+    //     Assert.False(CheckAllWinCondition(game));
+    //     Assert.False(_gameService.CheckDraw(game.Board));
     // }
     //
     // [Fact]
